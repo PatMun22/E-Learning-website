8b2ac4508fd93fc14a3c863db793e9b3bc271f4f
Patrick Munuku <wafulapatrick6@gmail.com>
2023-07-07 22:17:54 +0300

    Adding functionalities and login/registration forms

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let login = document.querySelector('.login');
+let register = document.querySelector('.register');
+
+login.addEventListener('onClick', () => {
+    login.style.display = 'block';
+    register.style.display = 'none';
+})
+
+register.addEventListener('onClick', () => {
+    register.style.display = 'block';
+    login.style.display = 'none';
+})
